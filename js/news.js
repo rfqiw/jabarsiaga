8937cf00b4187c62e595317333a5e3df43ee065a
rfqiw <rifqi.gusfiliandi2005@gmail.com>
2026-01-21 22:12:10 +0700

    Create news.js

---
+++ js/news.js
@@ -0,0 +1,10 @@
+// News Handler - Jabar Siaga Berita Kebencanaan
+
+class NewsManager {
+    constructor() {
+        this.newsData = [];
+        
+        // Updated news data with the 8 requested articles
+        this.newsData = [
+            {
+                title: '18 Bencana Terjang Jabar di Awal 2026, Karawang Jadi Titik Terpar
